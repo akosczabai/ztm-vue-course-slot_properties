71dc33dce109775f60d73539b209c4872b18ed40
akosczabai <czabai.akos@gmail.com>
2024-03-19 14:07:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <hello-world #default="{ user, favourites }">
+      <p>Hello {{ user.name }}. I like {{ favourites[0] }}</p>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
